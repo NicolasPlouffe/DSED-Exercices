c865671ce24ba43d49c34f4e11218d789df6bff2
NicolasPlouffe <nicolas.plouffe.qc.ca@gmail.com>
2025-05-05 15:13:19 -0400

    UDATE

--- exercice2/M01_DAL_Import_Munic_REST_JSON/Importation_Muni_REST_JSON.cs
+++ exercice2/M01_DAL_Import_Munic_REST_JSON/Importation_Muni_REST_JSON.cs
@@ -7,7 +7,7 @@ namespace M01_DAL_Import_Munic_REST_JSON;
 public class Importation_Muni_REST_JSON:IDepotImportationMunicipalites
 {
 
-    public static string uriRechercheParCodeGeographique = "https://www.donneesquebec.ca/{refCodeGeographique}/";
+    private static string uriRechercheParCodeGeographique = "https://www.donneesquebec.ca/recherche/api/action/datastore_search?resource_id=19385b4e-5503-4330-9e59-f998f5918363&limit=3000";
     public HttpClient httpClient;
     
     
@@ -25,7 +25,7 @@ public class Importation_Muni_REST_JSON:IDepotImportationMunicipalites
         this.httpClient.BaseAddress = new Uri("https://www.donneesquebec.ca/");
         this.httpClient.DefaultRequestHeaders.Accept.Clear();
         this.httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        Task<HttpResponseMessage> responseTask = httpClient.GetAsync(uriRechercheParCodeGeographique.Replace("{refCodeGeographique}",p_codeGeographique));
+        Task<HttpResponseMessage> responseTask = httpClient.GetAsync(uriRechercheParCodeGeographique);
         responseTask.Wait();
         HttpResponseMessage response = responseTask.Result;
 
@@ -39,4 +39,41 @@ public class Importation_Muni_REST_JSON:IDepotImportationMunicipalites
         
         return municipalites;
     }
+    
+    public IEnumerable<Municipalite> Lire(string p_jsonContent)
+    {
+        string json = File.ReadAllText(p_jsonContent);
+        Rootobject? root = JsonSerializer.Deserialize<Rootobject>(json);
+
+        return root!.result!.records!.Select(m =>
+            new Municipalite(
+                int.Parse(m!.mcode!),
+                m!.munnom!,
+                m!.mcourriel,
+                m!.mweb,
+                m!.datelec is not null ? DateOnly.FromDateTime(m.datelec.Value) : null
+
+            )
+        ).ToList();
+    }
+    internal class Rootobject
+    {
+        public Result? result { get; set; }
+    }
+
+    internal class Result
+    {
+        public Record[]? records { get; set; }
+    }
+
+    internal class Record
+    {
+        public string? mcode { get; set; }
+        public string? munnom { get; set; }
+        public string? mcourriel { get; set; }
+        public string? mweb { get; set; }
+        public DateTime? datelec { get; set; }
+    }
+    
+    
 }
