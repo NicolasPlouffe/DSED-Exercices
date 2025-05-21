b9edc7c3859030e77f9e58fff9bf217d11af51b9
NicolasPlouffe <nicolas.plouffe.qc.ca@gmail.com>
2025-05-21 03:26:04 -0400

    Ajout solution exercice

---
+++ DSED_M05_Ex01/DSED_M05_Ex01/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DSED_M05_Ex01.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
