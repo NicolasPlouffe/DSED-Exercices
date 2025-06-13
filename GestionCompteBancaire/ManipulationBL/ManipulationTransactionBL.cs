e8641cadcb2d465fc175440d82cd80789a640340
NicolasPlouffe <nicolas.plouffe.qc.ca@gmail.com>
2025-06-13 19:03:03 -0400

    MAJ des controllers API

--- GestionCompteBancaire/ManipulationBL/ManipulationTransactionBL.cs
+++ GestionCompteBancaire/ManipulationBL/ManipulationTransactionBL.cs
@@ -1,6 +1,61 @@
+using Entite.IDepot;
+
 namespace Entite.Manipulations;
 
 public class ManipulationTransactionBL
 {
+
+    #region Attributes
+    private readonly ITransactionDB _transactionDB;
+    private readonly ITransactionDepot m_TransactionDepot;
+    #endregion
     
+    
+    #region properties
+    
+    #endregion
+
+    #region Constructors
+
+    public ManipulationTransactionBL(ITransactionDB transactionDB, ITransactionDepot mTransactionDepot)
+    {
+        if (transactionDB is null) throw new ArgumentNullException(nameof(transactionDB));
+        if (mTransactionDepot is null) throw new ArgumentNullException(nameof(mTransactionDepot));
+        
+        this._transactionDB = transactionDB;
+        this.m_TransactionDepot = mTransactionDepot;
+    }
+    #endregion
+
+    #region CRUD Methods
+    // Creat Post
+    public void AjouterTransaction(TransactionEntite p_transaction)
+    {
+        this.m_TransactionDepot.CreerTransaction(p_transaction);
+    }
+    
+    //Read Get
+
+    public TransactionEntite ObtenirTransaction(Guid p_id)
+    {
+        return this.m_TransactionDepot.LireTransaction(p_id);
+    }
+    
+    //Uodate Put
+
+    void ModifierTransaction(TransactionEntite p_Entite)
+    {
+        this.m_TransactionDepot.MAJTransaction(p_Entite);
+    }
+    
+    #endregion
+    
+    #region methods
+    
+    #endregion
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
 }
